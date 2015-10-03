048060b4a1486d3ae33e280cebe9cb16c1317032
Helias <stefanoborzi32@gmail.com>
2015-10-03 23:53:53 +0200

    Importing project

---
+++ js/app/main.js
@@ -0,0 +1,20 @@
+/*jslint browser: true, white: true*/
+/*global angular, console, alert*/
+
+(function () {
+  'use strict';
+  var app = angular.module('arenastats', ['ui.router', 'ui.bootstrap', 'chieffancypants.loadingBar', 'tableSort']);
+
+  app.controller("HomeController", function($scope, $rootScope, $http) {
+
+    $http.get( app.api + "arena_team/" )
+      .success(function(data, status, header, config) {
+      $scope.teams = data;
+    })
+      .error(function(data, status, header, config) {
+      console.log("Error in ArenaStats $http.get");
+    });
+
+  });
+
+}());
